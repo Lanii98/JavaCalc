340c1ec93189c8c7bb013f1f17377b31f17c9732
basicJavaCoder <43179251+basicJavaCoder@users.noreply.github.com>
2024-02-22 17:06:46 +0000

    Refactor Factorial Method
    Refactored the Factorial Method to be a Ternary function instead,
    Just because I wanted to practise this type of code

--- Calculator/src/Factorial.java
+++ Calculator/src/Factorial.java
@@ -40,11 +40,9 @@ public class Factorial
     }
 
     private static int factorial(int n) {
-        if (n >  0) {
-            return n * factorial(n -  1);
-        } else {
-            return  1;
-        }
+        return n >  0
+            ? n * factorial(n -  1)
+            : 1;
     }
 
 }
